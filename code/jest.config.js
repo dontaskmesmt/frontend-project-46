53d660ebc8753eaf6ebcb1d15c12f0abe1ee21aa
Егор <123456789greens@gmail.com>
2025-12-17 18:36:22 +0300

    12345123

--- code/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  transform: {},
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-  roots: ['<rootDir>/../'],  // искать тесты на уровень выше
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  }
-}
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 export default {
   transform: {},
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
   testEnvironment: 'node',
-  moduleDirectories: ['node_modules', 'src'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  roots: ['<rootDir>/../'],  // искать тесты на уровень выше
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
 }
